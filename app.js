6f59d63cc24402a19d32cebdd4676ac9a2569c72
afisher93 <106286620+afisher93@users.noreply.github.com>
2022-08-31 17:39:17 -0500

    completed

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // TODO: Enable strict mode
-
+"use strict"
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+try {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
+  
+
+  } catch (error) {
+    console.error(error.message);
+    return JSON.parse(data)
+    throw new error(JSON)
+  }
+  
+   
   
   // TODO: if an exception is raised
   // print the error to the console
